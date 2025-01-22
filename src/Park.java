5f7eb58d2478d1d4659c495f552dfb29b3593cde
Igor Ogadzanyan <felarp@mail.ru>
2025-01-22 16:55:59 +0300

    done5

--- src/Park.java
+++ src/Park.java
@@ -8,14 +8,11 @@ public class Park {
             this.hours = hours;
             this.cost = cost;
         }
-
         public void printInfo() {
             System.out.printf("Аттракцион: %s, Время работы: %s, Стоимость: %.2f%n",
                     name, hours, cost);
         }
     }
-
-
     public static void main(String[] args) {
         Park park = new Park();
         Attraction[] attractions = {
@@ -23,7 +20,6 @@ public class Park {
                 park.new Attraction("Американские горки", "11:00-21:00", 500),
                 park.new Attraction("Комната страха", "12:00-22:00", 400)
         };
-
         for (Attraction attraction : attractions) {
             attraction.printInfo();
         }
